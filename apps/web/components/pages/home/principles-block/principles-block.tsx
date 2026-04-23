c3edf4b9dd6c7cd2eab0a573d29eb48db0992a5c
hieubdn <hieubdn@gmail.com>
2026-04-23 17:09:41 +0700

    fix CI

--- apps/web/components/pages/home/principles-block/principles-block.tsx
+++ apps/web/components/pages/home/principles-block/principles-block.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import styles from "./principles-block.module.scss";
-import { useLocaleText } from "@/components/layout/setting/translate/locale-provider";
 
 export default function PrinciplesBlock() {
-  const { t } = useLocaleText();
-
   return (
     <div className={styles.root}>
       
